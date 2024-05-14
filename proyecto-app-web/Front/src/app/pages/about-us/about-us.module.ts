47c0003da00f7f2eef93b454f65df9c3bb749af0
carlos linero <lineroacostacarlos@gmail.com>
2024-05-13 23:52:41 -0300

    add mobile app info page

---
+++ proyecto-app-web/Front/src/app/pages/about-us/about-us.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MobileInfoPageComponent } from './mobile-info-page/mobile-info-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    MobileInfoPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AboutUsModule { }
