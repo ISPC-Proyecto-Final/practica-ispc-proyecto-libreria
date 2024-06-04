871e312a81640288a0966f26fa793982bb63075d
Carla Ayelen Rios Suarez <carlaariossuarez@gmail.com>
2024-06-04 18:09:03 -0300

    add toast-module in app

--- proyecto-app-web/src/app/modules/layout/common-layouts.module.ts
+++ proyecto-app-web/src/app/modules/layout/common-layouts.module.ts
@@ -11,6 +11,8 @@ import { LoginModule } from '../login/login.module';
 import { ButtonCartComponent } from './button-cart/button-cart.component';
 // import { CommonComponentsModule } from '../common-components/common-components.module';
 import { FormsModule } from '@angular/forms';
+import { ToastComponent } from './toast/toast.component';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,7 @@ import { FormsModule } from '@angular/forms';
     SearchComponent,
     NavbarComponent,
     ButtonCartComponent,
+    ToastComponent,
   ],
   imports: [
     CommonModule,
@@ -26,12 +29,14 @@ import { FormsModule } from '@angular/forms';
     MatBadgeModule,
     // CommonComponentsModule,
     FormsModule,
+    NgbToastModule
   ],
   exports: [
     FooterComponent,
     HeaderMenuComponent,
     NavbarComponent,
     SearchComponent,
+    ToastComponent
   ],
 })
 export class CommonLayoutsModule {}
