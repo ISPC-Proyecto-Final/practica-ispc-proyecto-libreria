0b0ae98d650ce09bbcd81f10cffcbe9fb79eca04
Carla Ayelen Rios Suarez <carlaariossuarez@gmail.com>
2024-06-05 09:10:23 -0300

    implement toast message for multiples pages in mobile version

--- proyecto-app-web/src/app/pages/cart/selected-book-detail/selected-book-detail.component.ts
+++ proyecto-app-web/src/app/pages/cart/selected-book-detail/selected-book-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Book, SelectedBookDto } from 'src/app/models/book/book-model';
 import { CartService } from 'src/app/services/cart/cart.service';
+import { ToastService } from 'src/app/services/utils/toast.service';
 
 @Component({
   selector: 'app-selected-book-detail',
@@ -11,7 +12,10 @@ export class SelectedBookDetailComponent {
 
   @Input() book!: SelectedBookDto;
 
-  constructor(private CartService: CartService) { }
+  constructor(
+    private CartService: CartService,
+    private toastService: ToastService
+  ) { }
 
   onAddBook() {
     this.CartService.addBook(this.book);
@@ -23,6 +27,7 @@ export class SelectedBookDetailComponent {
 
   onRemoveBook() {
     this.CartService.removeBook(this.book.isbn);
+    this.toastService.createToast({type: 'bg-success', delay: 4000, message: 'Eliminaste libro del carrito'});
   }
 
 }
