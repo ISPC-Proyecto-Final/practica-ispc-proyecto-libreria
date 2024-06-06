7c684251a761b4d7996aafa77c7c243e8dc1cd5e
Carla Ayelen Rios Suarez <carlaariossuarez@gmail.com>
2024-06-06 10:32:31 -0300

    fix coupon models config

--- back/Scripts/Libreria/libreria_franklin/api.py
+++ back/Scripts/Libreria/libreria_franklin/api.py
@@ -53,10 +53,24 @@ class CustomAuthToken(ObtainAuthToken):
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({
-            'token': token.key,
             'id_user': user.id_user,
-            'email': user.email,
+            'last_login': user.last_login,
+            'is_superuser': user.is_superuser,
             'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'is_staff': user.is_staff,
+            'is_active': user.is_active,
+            'date_joined': user.date_joined,
+            'email': user.email,
+            'telephone_number': user.telephone_number,
+            'telephone_area_code': user.telephone_area_code,
+            'document': user.document,
+            'address_province': user.address_province,
+            'address_location': user.address_location,
+            'address_street': user.address_street,
+            'postal_code': user.postal_code,
+            'token': token.key,
         })
 
 class UserDeleteView(generics.DestroyAPIView):
